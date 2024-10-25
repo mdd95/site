1ae98ed82645add7f2e70e12cf79808ce7386d5b
mdd95 <enigmaticjayar@gmail.com>
2024-10-25 17:30:13 +0800

    feat: prosemirror

---
+++ src/lib/prosemirror/editor.ts
@@ -0,0 +1,51 @@
+import { EditorState } from 'prosemirror-state';
+import { EditorView } from 'prosemirror-view';
+import { schema } from 'prosemirror-schema-basic';
+import { baseKeymap, toggleMark } from 'prosemirror-commands';
+import { history, redo, undo } from 'prosemirror-history';
+import { keymap } from 'prosemirror-keymap';
+import type { MarkType } from 'prosemirror-model';
+
+import { getActiveMarksPlugin } from './plugins/get-activemarks-plugin.js';
+import type { ActiveMarks } from './plugins/get-activemarks-plugin.svelte.js';
+
+import 'prosemirror-view/style/prosemirror.css';
+
+export class Editor {
+	state: EditorState | undefined;
+	view: EditorView | undefined;
+	activeMarks: ActiveMarks | undefined;
+
+	bind(element: HTMLElement) {
+		this.state = EditorState.create({
+			schema,
+			plugins: [
+				history(),
+				keymap({ 'Mod-z': undo, 'Mod-y': redo }),
+				keymap(baseKeymap),
+				getActiveMarksPlugin(this.activeMarks)
+			]
+		});
+		this.view = new EditorView(element, {
+			state: this.state
+		});
+
+		return {
+			destroy: () => {
+				if (this.view) {
+					this.view.destroy();
+					this.view = undefined;
+					this.state = undefined;
+				}
+			}
+		};
+	}
+
+	toggleMark(fn: (e: typeof schema.marks) => MarkType) {
+		return () => {
+			if (this.view === undefined) return;
+			this.view.focus();
+			toggleMark(fn(schema.marks))(this.view.state, this.view.dispatch, this.view);
+		};
+	}
+}
---
+++ src/lib/prosemirror/index.ts
@@ -0,0 +1,2 @@
+export { Editor } from './editor.js';
+export { ActiveMarks } from './plugins/get-activemarks-plugin.svelte.js';
