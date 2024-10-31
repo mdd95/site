15319b37c48e214e42f10ae923642b08a7a42def
mdd95 <enigmaticjayar@gmail.com>
2024-10-31 12:54:50 +0800

    navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from '@/components/ui/button/index.js';
+	import LightSwitch from '@/components/light-switch.svelte';
+</script>
+
+<header class="sticky top-0 z-50 border-b border-border/40 bg-background">
+	<div class="container flex h-16 items-center justify-between">
+		<div class="mr-4">
+			<img src="/favicon.png" class="size-10" alt="logo" />
+		</div>
+		<div class="hidden grow md:flex md:items-center">
+			<Button variant="ghost">Home</Button>
+			<Button variant="ghost">Blog</Button>
+		</div>
+		<div class="flex items-center">
+			<LightSwitch />
+		</div>
+	</div>
+</header>
