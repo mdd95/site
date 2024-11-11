75a137628f98a7254924da088c6a24cffd2db4f8
mdd95 <enigmaticjayar@gmail.com>
2024-11-11 19:57:25 +0800

    extendable schema

--- src/lib/components/prosemirror/prosemirror.svelte
+++ src/lib/components/prosemirror/prosemirror.svelte
@@ -1,38 +1,54 @@
 <script lang="ts">
 	import { EditorState, Plugin } from 'prosemirror-state';
 	import { EditorView } from 'prosemirror-view';
-	import { schema } from 'prosemirror-schema-basic';
+	import { marks, nodes } from 'prosemirror-schema-basic';
 	import { keymap } from 'prosemirror-keymap';
 	import { baseKeymap, toggleMark } from 'prosemirror-commands';
 	import { history, redo, undo } from 'prosemirror-history';
-	import { DOMParser, type Schema } from 'prosemirror-model';
+	import { DOMParser, Schema, type MarkSpec, type NodeSpec } from 'prosemirror-model';
 	import type { HTMLAttributes } from 'svelte/elements';
 	import 'prosemirror-view/style/prosemirror.css';
 
 	type Props = HTMLAttributes<HTMLDivElement> & {
 		ref?: HTMLDivElement;
 		content?: string;
+		extend?: {
+			nodes?: Record<string, NodeSpec>;
+			marks?: Record<string, MarkSpec>;
+		};
 		plugins?: (params: { schema: Schema }) => Array<Plugin>;
 	};
 
 	const noop = () => [];
-	let { ref = $bindable(), content, plugins = noop, children, ...restProps }: Props = $props();
+	let {
+		ref = $bindable(),
+		content,
+		extend,
+		plugins = noop,
+		children,
+		...restProps
+	}: Props = $props();
 	let initial: HTMLDivElement;
 
 	$effect(() => {
+		const currentSchema = new Schema({
+			nodes: { ...nodes, ...(extend?.nodes || {}) },
+			marks: { ...marks, ...(extend?.marks || {}) }
+		});
+
 		const state = EditorState.create({
-			doc: DOMParser.fromSchema(schema).parse(initial),
-			schema,
+			doc: DOMParser.fromSchema(currentSchema).parse(initial),
+			schema: currentSchema,
 			plugins: [
 				keymap({
 					...baseKeymap,
-					'Mod-b': toggleMark(schema.marks.strong),
-					'Mod-i': toggleMark(schema.marks.em),
+					'Mod-b': toggleMark(currentSchema.marks.strong),
+					'Mod-i': toggleMark(currentSchema.marks.em),
 					'Mod-z': undo,
 					'Mod-y': redo
 				}),
 				history(),
-				...plugins({ schema })
+				...plugins({ schema: currentSchema })
 			]
 		});
 		const view = new EditorView(ref!, {
