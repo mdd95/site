8aa63a03deb5fe023147a59d6668314728bcaf02
mdd95 <enigmaticjayar@gmail.com>
2024-11-16 13:21:41 +0800

    refactor

--- src/routes/login/google/callback/+server.ts
+++ src/routes/login/google/callback/+server.ts
@@ -1,14 +1,9 @@
 import { error, redirect } from '@sveltejs/kit';
-import { ObjectParser } from '@pilcrowjs/object-parser';
 import { decodeIdToken } from 'arctic';
+import { eq } from 'drizzle-orm';
+import { db, table, generateId } from '@/server/db/index.js';
+import { createSession, generateSessionToken, setSessionTokenCookie } from '@/server/auth.js';
 import { google } from '@/server/oauth.js';
-import {
-	createSession,
-	generateSessionToken,
-	setSessionTokenCookie,
-	createUserFromGoogleId,
-	getUserIdFromGoogleId
-} from '@/server/auth.js';
 
 import type { OAuth2Tokens } from 'arctic';
 import type { RequestHandler } from './$types';
@@ -29,26 +24,47 @@ export const GET: RequestHandler = async (event) => {
 	let tokens: OAuth2Tokens;
 	try {
 		tokens = await google.validateAuthorizationCode(code, codeVerifier);
-	} catch (e) {
+	} catch (err) {
 		error(400, 'Please restart the process.');
 	}
 
-	const claims = decodeIdToken(tokens.idToken());
-	const claimsParser = new ObjectParser(claims);
+	type GoogleUser = {
+		sub: string;
+		name: string;
+		picture: string;
+		email: string;
+		given_name: string;
+		family_name: string;
+	};
 
-	const googleId = claimsParser.getString('sub');
-	const name = claimsParser.getString('name');
-	const picture = claimsParser.getString('picture');
-	const email = claimsParser.getString('email');
+	const info = decodeIdToken(tokens.idToken()) as GoogleUser;
 
-	let userId = await getUserIdFromGoogleId(googleId);
+	let user: table.User;
 
-	if (!userId) {
-		userId = await createUserFromGoogleId(googleId, email, name, picture);
+	try {
+		[user] = await db.select().from(table.user).where(eq(table.user.googleId, info.sub));
+
+		if (!user) {
+			[user] = await db
+				.insert(table.user)
+				.values({
+					id: generateId(15),
+					googleId: info.sub,
+					googleEmail: info.email,
+					googleName: info.name,
+					googleAvatarUrl: info.picture
+				})
+				.returning();
+		}
+	} catch (err) {
+		error(500, 'Internal server error');
+	}
+	if (!user) {
+		error(500, 'Internal server error');
 	}
 
 	const sessionToken = generateSessionToken();
-	const session = await createSession(sessionToken, userId);
+	const session = await createSession(sessionToken, user.id);
 
 	setSessionTokenCookie(event, sessionToken, session.expiresAt);
 	redirect(302, '/');
