4c46bb6812eb8bf6691961003007c5c5ca5d26b5
mdd95 <enigmaticjayar@gmail.com>
2024-11-16 20:22:36 +0800

    update ui in edit mode

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue, clsx } from 'clsx';
+import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { encodeBase64url } from '@oslojs/encoding';
+
+import type { ClassValue } from 'clsx';
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
+}
+
+export function useId(length: number) {
+	return encodeBase64url(crypto.getRandomValues(new Uint8Array(length)));
 }
--- src/routes/problem_set/[id]/edit/+page.svelte
+++ src/routes/problem_set/[id]/edit/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import { Button } from '@/components/ui/button';
 	import * as Breadcrumb from '@/components/ui/breadcrumb';
-
 	import Trash from 'svelte-radix/Trash.svelte';
 	import LightSwitch from '@/components/light-switch.svelte';
 	import Question from './question.svelte';
 	import Choice from './choice.svelte';
+	import { useId } from '@/utils.js';
 
 	import type { PageServerData } from './$types';
 	import 'prosemirror-view/style/prosemirror.css';
@@ -16,13 +16,32 @@
 
 	let { data }: Props = $props();
 
-	let problems = $state(
-		(data.result.content as {
-			id: string;
-			question: { value: string };
-			choices: { name: string; value: string }[];
-		}[]) || []
-	);
+	type Item = {
+		id: string;
+		question: { value: string };
+		choices: { name: string; value: string }[];
+		answer: 0 | 1 | 2 | 3;
+	};
+
+	let problems = $state<Item[]>(data.result.content ? data.result.content : [newItem()]);
+
+	function newItem(): Item {
+		return {
+			id: useId(6),
+			question: { value: '' },
+			choices: [
+				{ name: 'A', value: '' },
+				{ name: 'B', value: '' },
+				{ name: 'C', value: '' },
+				{ name: 'D', value: '' }
+			],
+			answer: 0
+		};
+	}
+
+	function deleteItem(id: string) {
+		problems = problems.filter((item) => item.id !== id);
+	}
 
 	async function save() {
 		const content = $state.snapshot(problems);
@@ -48,57 +67,48 @@
 	</div>
 </header>
 
-<div class="container">
-	<div class="py-4">
-		<Breadcrumb.Root>
-			<Breadcrumb.List>
-				<Breadcrumb.Item>
-					<Breadcrumb.Link href="/problem_set">Problem Sets</Breadcrumb.Link>
-				</Breadcrumb.Item>
-				<Breadcrumb.Separator />
-				<Breadcrumb.Item>
-					<Breadcrumb.Page>{data.result.title || 'Untitled Problem Set'}</Breadcrumb.Page>
-				</Breadcrumb.Item>
-			</Breadcrumb.List>
-		</Breadcrumb.Root>
+<div class="container space-y-4">
+	<Breadcrumb.Root class="mt-4">
+		<Breadcrumb.List>
+			<Breadcrumb.Item>
+				<Breadcrumb.Link href="/problem_set">Problem Sets</Breadcrumb.Link>
+			</Breadcrumb.Item>
+			<Breadcrumb.Separator />
+			<Breadcrumb.Item>
+				<Breadcrumb.Page class="max-w-80 truncate">
+					{data.result.title || 'Untitled Problem Set'}
+				</Breadcrumb.Page>
+			</Breadcrumb.Item>
+		</Breadcrumb.List>
+	</Breadcrumb.Root>
+
+	<div class="flex justify-end gap-2">
+		<Button onclick={save}>Save</Button>
+		<Button variant="secondary">Settings</Button>
 	</div>
 
-	{#each problems as prob, i}
-		<div class="px-2">
+	{#each problems as problem (problem.id)}
+		<div class="rounded-md border shadow-sm focus-within:ring-2 focus-within:ring-ring">
 			<div class="mb-4 flex justify-end">
-				<Button
-					variant="ghost"
-					size="icon"
-					onclick={() => {
-						problems = problems.filter((i) => {
-							const a = $state.snapshot(i).id;
-							const b = $state.snapshot(prob).id;
-							return a !== b;
-						});
-					}}
-				>
+				<Button variant="ghost" size="icon" onclick={() => deleteItem(problem.id)}>
 					<Trash />
 				</Button>
 			</div>
 
-			<div
-				class="mb-4 rounded-md border p-4 shadow-sm focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-1"
-			>
-				<Question bind:htmlContent={prob.question}>
-					{@html $state.snapshot(prob.question.value)}
+			<div class="p-4">
+				<Question bind:htmlContent={problem.question}>
+					{@html $state.snapshot(problem.question.value)}
 				</Question>
 			</div>
 
-			<div class="grid grid-cols-1 gap-4 md:grid-cols-2">
-				{#each prob.choices as choice, i}
-					<div
-						class="grid grid-cols-[auto_1fr] rounded-md border p-4 shadow-sm focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-1"
-					>
+			<div class="grid grid-cols-1 gap-2 md:grid-cols-2">
+				{#each problem.choices as choice, i}
+					<div class="grid grid-cols-[auto_1fr] p-4">
 						<div class="mr-4">
 							<input type="radio" name="problem-1" />
 							<label for="" class="text-muted-foreground">{choice.name}</label>
 						</div>
-						<Choice bind:htmlContent={prob.choices[i]}>
+						<Choice bind:htmlContent={problem.choices[i]}>
 							{@html $state.snapshot(choice.value)}
 						</Choice>
 					</div>
@@ -107,22 +117,8 @@
 		</div>
 	{/each}
 
-	<div class="flex gap-4">
-		<Button
-			onclick={() => {
-				problems.push({
-					id: crypto.randomUUID(),
-					question: { value: '' },
-					choices: [
-						{ name: 'A', value: '' },
-						{ name: 'B', value: '' },
-						{ name: 'C', value: '' },
-						{ name: 'D', value: '' }
-					]
-				});
-			}}>+</Button
-		>
-		<Button onclick={save}>Save</Button>
+	<div>
+		<Button variant="outline" class="w-full" onclick={() => problems.push(newItem())}>+</Button>
 	</div>
 </div>
 
