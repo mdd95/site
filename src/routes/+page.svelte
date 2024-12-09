70ec9d3a61b32b7077f2c5d2806238fd1d912980
mdd95 <enigmaticjayar@gmail.com>
2024-12-09 22:56:10 +0800

    default primary color palette

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { Button } from '@/components/ui/button';
+	import { getThemeContext } from '@/theme-mode.svelte';
+
+	const theme = getThemeContext();
+</script>
+
+<header class="border-primary-100 dark:border-primary-950 sticky top-0 z-50 border-b">
+	<div class="container mx-auto flex h-16 items-center justify-between">
+		<Button onclick={() => (theme.mode = theme.mode === 'dark' ? 'light' : 'dark')}>
+			Toggle dark mode
+		</Button>
+	</div>
+</header>
+
+<div class="container mx-auto py-12">
+	<h1>Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+</div>
