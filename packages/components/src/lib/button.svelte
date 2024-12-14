76ed256e5cf929f61c071c695e4406719340fb01
mdd95 <enigmaticjayar@gmail.com>
2024-12-14 12:06:51 +0800

    components sub repo

---
+++ packages/components/src/lib/button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import type { HTMLAttributes } from "svelte/elements";
+
+  type Props = HTMLAttributes<HTMLButtonElement> & {
+    children?: Snippet;
+  };
+
+  let { children, ...restProps }: Props = $props();
+</script>
+
+<button {...restProps}>
+  {@render children?.()}
+</button>
