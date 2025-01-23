86a3a6cd65082afd17f4309ffe23ea056769e233
mdd95 <enigmaticjayar@gmail.com>
2025-01-23 20:45:51 +0800

    update styling

--- packages/components/src/lib/components/button.svelte
+++ packages/components/src/lib/components/button.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
   import type { HTMLAnchorAttributes, HTMLButtonAttributes } from 'svelte/elements';
-  import type { WithElementRef } from 'bits-ui';
 
-  type Props = WithElementRef<HTMLButtonAttributes> & WithElementRef<HTMLAnchorAttributes>;
-
-  let { ref = $bindable(null), children, href = undefined, ...restProps }: Props = $props();
+  type Props = HTMLButtonAttributes & HTMLAnchorAttributes;
+  let { children, href = undefined, ...restProps }: Props = $props();
 </script>
 
 {#if href}
-  <a bind:this={ref} {href} {...restProps}>{@render children?.()}</a>
+  <a {href} {...restProps}>
+    {@render children?.()}
+  </a>
 {:else}
-  <button bind:this={ref} {...restProps}>{@render children?.()}</button>
+  <button {...restProps}>
+    {@render children?.()}
+  </button>
 {/if}
 
 <style>
@@ -22,26 +24,26 @@
     display: inline-flex;
     align-items: center;
     justify-content: center;
-    border-radius: var(--border-radius-default);
-    box-shadow: var(--shadow-default);
-    background-color: var(--scope-background);
-    color: var(--scope-foreground);
-    font-size: 0.875rem;
-    line-height: 1.25rem;
+    border-radius: var(--radius);
+    box-shadow: var(--shadow);
+    background-color: color-mix(in oklab, var(--background) 90%, var(--text));
+    color: var(--text);
     white-space: nowrap;
     cursor: pointer;
-    transition-property: var(--transition-property-colors);
-    transition-duration: var(--transition-duration-default);
-    transition-timing-function: var(--transition-timing-function-default);
+    transition-property: var(--transition-colors);
+    transition-duration: var(--transition-duration);
+    transition-timing-function: var(--transition-easing);
 
     &:disabled {
-      background-color: color-mix(in oklab, var(--scope-background), transparent 50%);
+      background-color: color-mix(in oklab, var(--background) 50%, transparent);
       cursor: auto;
       pointer-events: none;
     }
 
-    &:hover {
-      background-color: color-mix(in oklab, var(--scope-background), transparent 15%);
+    @media (hover: hover) {
+      &:hover {
+        background-color: color-mix(in oklab, var(--background) 85%, var(--text));
+      }
     }
 
     & :global(svg) {
@@ -52,25 +54,14 @@
     }
   }
 
-  a,
-  button {
-    --scope-background: var(--color-primary-500);
-    --scope-foreground: var(--color-primary-100);
-  }
-
   .ghost {
-    --scope-background: var(--background-default);
-    --scope-foreground: var(--foreground-default);
     box-shadow: none;
-    &:hover {
-      --scope-foreground: var(--color-primary-100);
-      background-color: var(--color-primary-500);
-    }
+    background-color: transparent;
   }
 
   .destructive {
-    --scope-background: oklch(0.637 0.237 25.331);
-    --scope-foreground: oklch(1 0 0);
+    --background: oklch(0.637 0.237 25.331);
+    --text: #fff;
   }
 
   .sm {
