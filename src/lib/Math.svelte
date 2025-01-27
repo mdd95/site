a6b7fc411b33b0e150f5ec08f740758415c44451
mdd95 <enigmaticjayar@gmail.com>
2025-01-27 18:20:59 +0800

    math-preprocess

---
+++ src/lib/Math.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import 'katex/dist/katex.css';
+  import katex from 'katex';
+
+  type Props = {
+    render: string;
+    display?: boolean;
+  };
+  let { render, display = true }: Props = $props();
+  let ref: HTMLElement;
+
+  $effect(() => {
+    katex.render(render.trim(), ref, {
+      displayMode: display,
+      throwOnError: false
+    });
+  });
+</script>
+
+<div bind:this={ref}></div>
+
+<style>
+  div {
+    font-size: 0.875em;
+  }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: [vitePreprocess(), mathPreprocess()],
 
   kit: {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
@@ -14,5 +14,36 @@ const config = {
     adapter: adapter()
   }
 };
+
+function mathPreprocess(name = 'Math', path = '$lib/Math.svelte') {
+  return {
+    name: 'math-preprocess',
+    markup({ content }) {
+      const scriptTag = /<script(?![^>]*\bmodule\b)[^>]*>/g;
+      const inlineMath = /\{\`\$ (.+?) \$\`\}/g;
+      const displayMath = /\{\`\$\$(.*?)\$\$\`\}/gs;
+      const importMath = `import ${name} from "${path}";`;
+      const importCheck = new RegExp(`import\\s+${name}\\s+from\\s+['"]${path}['"]`);
+
+      if (inlineMath.test(content) || displayMath.test(content)) {
+        if (!scriptTag.test(content)) {
+          content = `<script>\n\t${importMath}\n</script>\n\n${content}`;
+        } else if (!importCheck.test(content)) {
+          content = content.replace(scriptTag, (match) => {
+            return `${match}\n\t${importMath}`;
+          });
+        }
+        content = content
+          .replace(inlineMath, (_, p) => {
+            return `<Math render={\`${p}\`} display={false} />`;
+          })
+          .replace(displayMath, (_, p) => {
+            return `<Math render={\`${p.trim()}\`} display={true} />`;
+          });
+      }
+      return { code: content };
+    }
+  };
+}
 
 export default config;
