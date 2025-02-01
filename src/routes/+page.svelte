2da8cf54f539a31140da06dea7cfba75ef583341
mdd95 <enigmaticjayar@gmail.com>
2025-02-01 14:06:58 +0800

    try remark plugins

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-{`$$ \\frac{df}{dt} = \\lim_{h\\rarr0} {\\frac{f(t+h)-f(t)}{h}} $$`}
--- svelte.config.js
+++ svelte.config.js
@@ -1,45 +1,52 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
+import remarkMath from 'remark-math';
+import rehypeKatexSvelte from 'rehype-katex-svelte';
+// import katex from 'katex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [vitePreprocess(), mdsvex(), mathPreprocess()],
+  preprocess: [
+    // mathPreprocess(),
+    vitePreprocess(),
+    mdsvex({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatexSvelte]
+    })
+  ],
   kit: {
     adapter: adapter()
   },
   extensions: ['.svelte', '.svx']
 };
 
-function mathPreprocess(name = 'Math', path = '$lib/Math.svelte') {
-  return {
-    name: 'math-preprocess',
-    markup({ content }) {
-      const scriptTag = /<script(?![^>]*\bmodule\b)[^>]*>/g;
-      const inlineMath = /\{\`\$ (.+?) \$\`\}/g;
-      const displayMath = /\{\`\$\$(.*?)\$\$\`\}/gs;
-      const importMath = `import ${name} from "${path}";`;
-      const importCheck = new RegExp(`import\\s+${name}\\s+from\\s+['"]${path}['"]`);
+// function renderMath(tex, displayMode = false) {
+//   const output = katex.renderToString(tex, {
+//     displayMode,
+//     throwOnError: false
+//   });
+//   return String.raw`{@html \`${output}\`}`;
+// }
 
-      if (inlineMath.test(content) || displayMath.test(content)) {
-        if (!scriptTag.test(content)) {
-          content = `<script>\n\t${importMath}\n</script>\n\n${content}`;
-        } else if (!importCheck.test(content)) {
-          content = content.replace(scriptTag, (match) => {
-            return `${match}\n\t${importMath}`;
-          });
-        }
-        content = content
-          .replace(inlineMath, (_, p) => {
-            return `<Math render={\`${p}\`} display={false} />`;
-          })
-          .replace(displayMath, (_, p) => {
-            return `<Math render={\`${p.trim()}\`} display={true} />`;
-          });
-      }
-      return { code: content };
-    }
-  };
-}
+// function mathPreprocess() {
+//   return {
+//     name: 'math-preprocess',
+//     markup({ content, filename }) {
+//       if (filename.endsWith('.svx')) {
+//         const inline = /\$(?!\s)(.+?)(?<!\s)\$/g;
+//         const block = /\$\$([\s\S]+?)\$\$/g;
+
+//         if (inline.test(content) || block.test(content)) {
+//           console.log(content);
+//           content = content
+//             .replace(block, (_, tex) => renderMath(tex, true))
+//             .replace(inline, (_, tex) => renderMath(tex));
+//           return { code: content };
+//         }
+//       }
+//     }
+//   };
+// }
 
 export default config;
