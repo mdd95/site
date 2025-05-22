c0196e8d8a9101444487d3fb8f28497324647b51
mdd95 <enigmaticjayar@gmail.com>
2025-05-22 21:01:51 +0800

    stopwatch timer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
 	import '../app.css';
-	import { ModeWatcher } from 'mode-watcher';
+	import { ModeWatcher, toggleMode } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import Moon from '@lucide/svelte/icons/moon';
+	import Sun from '@lucide/svelte/icons/sun';
 
 	let { children } = $props();
 </script>
 
 <ModeWatcher />
+<header class="container flex justify-between p-4">
+	<div></div>
+	<div>
+		<Button variant="ghost" size="icon" onclick={() => toggleMode()}>
+			<Moon class="size-4 not-dark:hidden" />
+			<Sun class="size-4 dark:hidden" />
+			<span class="sr-only">Toggle Dark Mode</span>
+		</Button>
+	</div>
+</header>
 {@render children?.()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,3 @@
-<script>
-	import { toggleMode } from 'mode-watcher';
-	import { Button } from '$lib/components/ui/button/index.js';
-	import Moon from '@lucide/svelte/icons/moon';
-	import Sun from '@lucide/svelte/icons/sun';
-</script>
-
-<header class="container flex justify-between p-4">
-	<div></div>
-	<div>
-		<Button variant="ghost" size="icon" onclick={() => toggleMode()}>
-			<Moon class="size-4 not-dark:hidden" />
-			<Sun class="size-4 dark:hidden" />
-			<span class="sr-only">Toggle Dark Mode</span>
-		</Button>
-	</div>
-</header>
 <main class="prose dark:prose-invert mx-auto px-4">
 	<h1>Welcome to SvelteKit</h1>
 	<p>
