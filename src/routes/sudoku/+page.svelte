1e7683a3d227e1b31042eb1a2fb9c2daec053f12
mdd95 <enigmaticjayar@gmail.com>
2025-11-19 20:51:10 +0800

    update visuals

--- src/routes/sudoku/+page.svelte
+++ src/routes/sudoku/+page.svelte
@@ -7,6 +7,13 @@
 	let grid = $state.raw(new Uint8Array(81));
 	let loading = $state(false);
 
+	const size = 1080;
+	const spacing = 4;
+	const spacing_mult = [1, 2, 3, 5, 6, 7, 9, 10, 11];
+	const cell_size = (size - 12 * spacing) / 9;
+
+	let selection = $state<number | null>(null);
+
 	onMount(() => {
 		game = new Sudoku();
 
@@ -14,6 +21,8 @@
 			grid = e.data[0];
 			loading = false;
 		});
+
+		generate();
 	});
 
 	function generate() {
@@ -22,23 +31,45 @@
 	}
 </script>
 
-<div class="grid">
-	{#each grid as m, i (i)}
-		<div class="cell">{m === 0 ? ' ' : m}</div>
+<svg xmlns="" viewBox="0 0 {size} {size}" class="sudoku">
+	<rect x="0" y="0" width={size} height={size} fill="#3a3a3a" />
+	{#each { length: 81 }, i}
+		{@const n = grid[i]}
+		{@const c = i % 9}
+		{@const r = Math.floor(i / 9)}
+		{@const x = c * cell_size + spacing * spacing_mult[c]}
+		{@const y = r * cell_size + spacing * spacing_mult[r]}
+		<rect
+			{x}
+			{y}
+			width={cell_size}
+			height={cell_size}
+			fill={selection === i ? '#dadada' : '#ffffff'}
+			onclick={() => (selection = i)}
+		/>
+		<text
+			x={x + cell_size * 0.5}
+			y={y + cell_size * 0.5}
+			fill="#000000"
+			font-family="Arial, sans-serif"
+			font-size="82"
+			dominant-baseline="central"
+			text-anchor="middle"
+		>
+			{n === 0 ? '' : n}
+		</text>
 	{/each}
-</div>
+</svg>
 <button onclick={generate} disabled={loading}>Generate</button>
 
 <style>
-	.grid {
-		display: grid;
-		grid-template-columns: repeat(9, 2rem);
-		grid-template-rows: repeat(9, 2rem);
+	svg.sudoku {
+		width: min(100%, 32rem);
+		aspect-ratio: 1 / 1;
+		user-select: none;
 	}
 
-	.cell {
-		border: 1px solid #121212;
-		display: grid;
-		place-items: center;
+	svg.sudoku text {
+		pointer-events: none;
 	}
 </style>
