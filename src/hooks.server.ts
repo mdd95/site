872fe2080538e09a5e8f75f8ed40302c1d5bdf08
mdd95 <enigmaticjayar@gmail.com>
2026-05-15 16:54:01 +0800

    restore default

--- src/hooks.server.ts
+++
@@ -1,9 +0,0 @@
-import { createInitialModeExpression } from 'mode-watcher';
-import type { Handle } from '@sveltejs/kit';
-
-export const handle: Handle = async ({ event, resolve }) => {
-	return resolve(event, {
-		transformPageChunk: ({ html }) =>
-			html.replace('%modewatcher.snippet%', createInitialModeExpression())
-	});
-};
