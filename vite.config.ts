2ded104f79982f14665be283f25082ad8415569e
mdd95 <enigmaticjayar@gmail.com>
2026-06-06 14:35:45 +0800

    Update svelte

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,24 @@
+import adapter from '@sveltejs/adapter-node';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit({
+			compilerOptions: {
+				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+				runes: ({ filename }) =>
+					filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
+				experimental: { async: true }
+			},
+			adapter: adapter(),
+			experimental: { remoteFunctions: true },
+			typescript: {
+				config: (config) => ({
+					...config,
+					include: [...config.include, '../drizzle.config.ts']
+				})
+			}
+		})
+	]
 });
