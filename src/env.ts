0c5e2a9ce33d015308a79ed72325a69a764dd5a2
mdd95 <enigmaticjayar@gmail.com>
2026-07-12 17:16:32 +0800

    feat: add descriptions and schemas for environment variables

--- src/env.ts
+++ src/env.ts
@@ -1,13 +1,23 @@
 import { defineEnvVars } from '@sveltejs/kit/hooks';
+import * as v from 'valibot';
 
 export const variables = defineEnvVars({
 	DATABASE_URL: {
-		public: false
+		description: 'Connection string for postgres database',
+		public: false,
+		schema: v.pipe(v.string(), v.startsWith('postgres://')),
+		static: true
 	},
 	ORIGIN: {
-		public: true
+		description: 'Public base URL of the application',
+		public: true,
+		schema: v.pipe(v.string(), v.url()),
+		static: true
 	},
 	SECRET: {
-		public: false
+		description: 'Secret key for signing cryptographic tokens',
+		public: false,
+		schema: v.pipe(v.string(), v.minLength(32)),
+		static: true
 	}
 });
