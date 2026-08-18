691f27c781b845ec780894a87e2c2ef90bbfc43d
mdd95 <enigmaticjayar@gmail.com>
2026-08-18 19:44:57 +0800

    try `LiquidEther.svelte` component & update dependencies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,25 @@
 	import { resolve } from '$app/paths';
 	import { getUser, signOut } from '$lib/remote/auth.remote.js';
 	import { mode, toggleMode } from 'mode-watcher';
+	import LiquidEther from '$lib/components/LiquidEther.svelte';
 </script>
 
+<div style="width:100%;height:600px;position:relative;">
+	<LiquidEther
+		colors={['#FF3E00', '#FF8A4C', '#FFB089']}
+		mouseForce={20}
+		cursorSize={100}
+		isViscous={true}
+		viscous={30}
+		iterationsViscous={32}
+		iterationsPoisson={32}
+		resolution={0.5}
+		isBounce={false}
+		autoDemo={true}
+		autoSpeed={0.5}
+		autoIntensity={2.2}
+	/>
+</div>
 <div class="container">
 	<header>
 		<div></div>
@@ -32,7 +49,7 @@
 
 		<div style="margin-top: 1rem;">
 			<button class="primary">Get Started</button>
-			<button onclick={toggle}>Toggle</button>
+			<button onclick={toggleMode}>Toggle</button>
 		</div>
 	</main>
 </div>
