a35d63cc8581f86c0f1762790b649656835fe1d4
mdd95 <enigmaticjayar@gmail.com>
2026-08-29 21:29:53 +0800

    init: navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import '@fontsource-variable/inter/wght.css';
+	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Navbar from '$lib/components/Navbar.svelte';
+</script>
+
+<svelte:head>
+	<meta name="description" content="Mark's personal website." />
+	<title>mjayar</title>
+</svelte:head>
+
+<Navbar />
